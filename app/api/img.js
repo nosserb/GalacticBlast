c2142046e1e90186c802d1cee2fe51aa6d4695cc
nosserb <guylann.bresson.gb@gmail.com>
2025-12-01 16:20:23 +0100

    fix: route.js

--- app/api/img.js
+++ app/api/img.js
@@ -1,26 +1,21 @@
-import { createCanvas } from "canvas";
+import { createCanvas } from "@napi-rs/canvas";
 
 export default function handler(req, res) {
-
   const texte = req.query.texte || "Aucun texte";
-  
-  // Taille de l'image
+
   const width = 800;
   const height = 200;
 
   const canvas = createCanvas(width, height);
   const ctx = canvas.getContext("2d");
 
-  // Fond blanc
   ctx.fillStyle = "#ffffff";
   ctx.fillRect(0, 0, width, height);
 
-  // Texte
   ctx.fillStyle = "#000000";
   ctx.font = "bold 40px Arial";
   ctx.fillText(texte, 50, 120);
 
-  // Retourner l'image
   res.setHeader("Content-Type", "image/png");
-  res.send(canvas.toBuffer());
+  res.send(canvas.toBuffer("image/png"));
 }
